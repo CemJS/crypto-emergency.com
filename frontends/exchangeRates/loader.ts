c8260b7eb4f6b39477da9adc31063e1ef0015747
maksdrop <maksondmb2018@gmail.com>
2023-07-05 18:04:33 +0300

    add exchange-rates

---
+++ frontends/exchangeRates/loader.ts
@@ -0,0 +1,10 @@
+export const loader = function () {
+  new EventSource('/api/events/CoinsCourse').addEventListener('message', ({ data }) => {
+    let records = JSON.parse(data)
+
+    this.Static.records = records
+    this.init()
+  })
+
+  this.Static.title = "Курсы валют"
+}
