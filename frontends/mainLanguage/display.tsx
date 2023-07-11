4fd23baf2805743f6e514cf0cd5c722c88f43eb4
maksdrop <maksondmb2018@gmail.com>
2023-07-11 13:30:09 +0300

    new microfront "mainLanguage"

---
+++ frontends/mainLanguage/display.tsx
@@ -0,0 +1,116 @@
+import { Cemjsx } from "cemjs-all"
+
+const language = [
+  {
+    "lang": "China",
+    "lang_orig": "俄语",
+  },
+  {
+    "lang": "Deutsch",
+    "lang_orig": "Deutsch",
+  },
+  {
+    "lang": "Greek",
+    "lang_orig": "Ελληνικά",
+  },
+  {
+    "lang": "English",
+    "lang_orig": "English",
+  },
+  {
+    "lang": "Spanish",
+    "lang_orig": "Español",
+  },
+  {
+    "lang": "French",
+    "lang_orig": "Français",
+  },
+  {
+    "lang": "Hebrew",
+    "lang_orig": "רובית",
+  },
+  {
+    "lang": "Armenian",
+    "lang_orig": "ռուսերեն",
+  },
+  {
+    "lang": "Indonesian",
+    "lang_orig": "Bahasa Indonesia",
+  },
+  {
+    "lang": "Italian",
+    "lang_orig": "Italiano",
+  },
+  {
+    "lang": "Japanese",
+    "lang_orig": "日本語",
+  },
+  {
+    "lang": "polish",
+    "lang_orig": "polszczyzna",
+  },
+  {
+    "lang": "Portuguese",
+    "lang_orig": "Português",
+  },
+  {
+    "lang": "Russian",
+    "lang_orig": "Русский",
+  },
+  {
+    "lang": "Turkish",
+    "lang_orig": "Türkçe",
+  },
+  {
+    "lang": "Український",
+    "lang_orig": "Український",
+  }
+]
+
+export const display = function () {
+  return (
+    <div class="modal modal_open">
+      <div class="modal__black" 
+        onclick={() => {
+          setTimeout(() => {
+            this.clearData()
+          }, 5);
+        }}
+      />
+      <div class="modal__container">
+        <header class="modal__header">
+          <h4 class="language__title">
+            Выбрать язык
+          </h4>
+          <button
+            class="modal__close"
+            type="button"
+            onclick={() => {
+              setTimeout(() => {
+                this.clearData()
+              }, 5);
+            }}
+          >
+          </button>
+        </header>
+        <div class="modal__body language__container">
+          <ul class="language__list">
+            {
+              language.map((item) => {
+                return (
+                  <li class="language__item">
+                    <a>
+                      <span>
+                        {item.lang_orig}
+                      </span>
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
