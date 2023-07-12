11fb6764f6fef63ea8d88be9b8e28ffb53d8803f
Betarost <betarost@yandex.ru>
2023-07-12 12:31:05 +0300

    Update esbuild.mjs

--- esbuild.mjs
+++ esbuild.mjs
@@ -162,7 +162,6 @@ const start = async function () {
             if (!haveChange && req.url !== "/esbuild" && !req.url.startsWith("/assets")) {
                 req.url = "/"
             }
-            console.log('=cddf8b=', req.url)
             proxy.web(req, res, { target: `http://${options.hostname}:${options.port}`, changeOrigin: true });
 
             proxy.on('error', function (err, req, res) {
