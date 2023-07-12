20d95ca05f947f35f6d3221b51f39ab79b73fcef
Betarost <betarost@yandex.ru>
2023-07-12 10:40:25 +0300

    Add event

--- frontends/courseLine/loader.ts
+++ frontends/courseLine/loader.ts
@@ -1,6 +1,6 @@
 export const loader = function () {
 
-  new EventSource('/api/events/CoinsCourse').addEventListener('message', ({ data }) => {
+  this.eventSource(`/api/events/CoinsCourse?uuid=${this.Variable.myInfo?.uuid}`, ({ data }) => {
     let records = JSON.parse(data)
 
     this.Static.records = records
