8ce08fbca1cbf2cd5be5d5f18a773e9287f4bf7d
maksdrop <maksondmb2018@gmail.com>
2023-07-13 11:16:52 +0300

    page about

---
+++ frontends/about/display.tsx
@@ -0,0 +1,18 @@
+import { Cemjsx } from "cemjs-all"
+
+export const display = function () {
+  return (
+    <div class="page page__container">
+      <div class="about">
+        <div class="about__whome whome">
+          <div class="whome__inner">
+            <h2>Crypto Emergency</h2>
+            <p>Объединяем криптоэнтузиастов всего мира на единой многофункциональной платформе Crypto Emergency, которая даст им все необходимые инструменты для общения, обучения, заработка и создания собственного контента. </p>
+          </div>
+        </div>
+        .about
+      </div>
+    </div>
+  )
+
+}
