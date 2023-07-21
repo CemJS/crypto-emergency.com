dcc934612c5138c7c7a1c448ae45f5757bd3b78f
maksdrop <maksondmb2018@gmail.com>
2023-07-21 15:28:19 +0300

    new modal recoverPass

---
+++ frontends/modalRecoverPass/display.tsx
@@ -0,0 +1,57 @@
+import { Cemjsx } from "cemjs-all"
+
+export const display = function () {
+  return (
+    <div class="modal modal_open modal_fullscreen">
+      <div class="modal__black"
+        onclick={() => {
+          setTimeout(() => {
+            this.clearData()
+          }, 5);
+        }}
+      />
+      <div class="modal__container reset-password">
+        <header class="modal__header reset-password__header">
+          <h2 class="registration__title">
+            Восстановение пароля
+          </h2>
+          <button
+            class="modal__close"
+            type="button"
+            onclick={() => {
+              setTimeout(() => {
+                this.clearData()
+              }, 5);
+            }}
+          />
+        </header>
+        <div class="modal__body">
+          <form class="reset-password__form">
+            <label>E-mail</label>
+            <div class="reset-password__form_input">
+              <input type="text" placeholder="Введите ваш e-mail"
+                oninput={(e) => {
+                  if (this._fn.validateEmail(e.target.value)) {
+                    this.Static.isValid = true
+                  } else {
+                    this.Static.isValid = false
+                  }
+                  this.init()
+                }}
+              />
+            </div>
+            <div class="reset-password__form_footer">
+              <button class={["button", "button_gradient",
+                  !this.Static.isValid ? "button_inactive" : null
+                ]}
+              >
+                <span>Восстановить</span>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+
+}
---
+++ frontends/modalRecoverPass/functions.ts
@@ -0,0 +1,11 @@
+const fn = {
+  validateEmail: function (email) {
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    );
+  } 
+}
+
+export { fn }
