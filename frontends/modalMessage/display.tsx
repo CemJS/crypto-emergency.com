fc116c6bef9dbfc5afc299d63e13a1675a74cc9e
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-07-24 11:50:19 +0300

    +

--- frontends/modalMessage/display.tsx
+++ frontends/modalMessage/display.tsx
@@ -2,12 +2,32 @@ import { Cemjsx } from "cemjs-all"
 
 export const display = function () {
   return (
-    <div class="modalWindow">
-      <div class="modalWindow_body">
+    <div 
+      class="modalWindow"
+      onclick={(e)=>{
+        if(e.target === this.Ref.modalBody){
+          setTimeout(()=>{
+            this.clearData()
+          }, 5)
+        }
+      }}  
+    >
+      <div class="modalWindow_body" ref="modalBody">
         <div class="modalWindow_content">
-          <header class="modalWindow_header"></header>
-          <main class="modalWindow_main">hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</main>
-          <footer class="modalWindow_footer"></footer>
+          <button class="modalWindow_button_close">X</button>
+          <header class="modalWindow_header">
+            <h3 class="modalWindow_header_title">Связь с нами</h3>
+            <span class="modalWindow_header_subtitle">Напиши нам, и мы с Вами свяжемся!</span>
+          </header>
+          <main class="modalWindow_main">
+            <div>
+              <label>Ваше имя</label>
+              <input placeholder="Ваше имя"></input>
+            </div>
+          </main>
+          <footer class="modalWindow_footer">
+            <button>Отправить</button>
+          </footer>
         </div>
       </div>
     </div>
