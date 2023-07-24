c5b3726c560d619c77371fd9d07d30c3069467df
Betarost <betarost@yandex.ru>
2023-07-24 15:21:31 +0300

    rename json file

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 import { initMap } from 'cemjs-all'
 import cemconfig from './cemconfig.json'
-import microFrontends from './microFrontends.json'
+import frontends from './frontends.json'
 import services from './services.json'
 
-cemconfig.microFrontends = microFrontends
+cemconfig.microFrontends = frontends
 cemconfig.services = services
 export default function () {
     initMap(cemconfig)
