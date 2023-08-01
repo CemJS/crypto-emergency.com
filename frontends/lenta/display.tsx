776a0605a975d2d860919c63a420ed34bd4a5f3a
maksdrop <maksondmb2018@gmail.com>
2023-08-01 18:16:16 +0300

    fix lenta

--- frontends/lenta/display.tsx
+++ frontends/lenta/display.tsx
@@ -1,12 +1,41 @@
 import { Cemjsx } from "cemjs-all"
-import test from "@images/lenta/test.png"
+import videoBackground from "@images/lenta/video_background.png"
 import dislike from "@svg/lenta/dislike.svg"
 import like from "@svg/lenta/like.svg"
 import comments from "@svg/lenta/comments.svg"
 import views from "@svg/lenta/views.svg"
 
 export const display = function () {
-  console.log('=84221a=', this.Static.records)
+  console.log('=cbf168=',this.Static.records)
+
+  const lentaMedia = function ( item ) {
+
+    if (item.length == 0) { return null }
+    let arrVideo = item.filter((item) => item.type === "video");
+    let arrImage = item.filter((item) => item.type === "image");
+
+    if (!arrVideo.length && arrImage.length == 1) {
+      return (
+        <img src={`/assets/upload/posts/${item[0]?.name}`} />
+      )
+    }
+
+    if (!arrImage.length && arrVideo.length == 1) {
+      return (
+        <div class="lenta__item_video">
+          {
+            item[0].previewName
+            ?
+            <img src={`/assets/upload/posts/${item[0]?.previewName}`} />
+            :
+            <img src={videoBackground} />
+          }
+        </div>
+        
+      )
+    }
+  }
+
   return (
     <div class="lenta">
       <div class="lenta__container">
@@ -16,10 +45,23 @@ export const display = function () {
               <div class="lenta__item">
                 <div class="lenta__item_wrapper">
                   <div class="lenta__item_body">
-                    <img src={test} alt="" />
+                    {
+                      item.Media.length > 0
+                        ?
+                        lentaMedia(item.Media)
+                        :
+                        null
+                    }
+                    {/* <img src={`/assets/upload/posts/${item.Media[0]?.name}`} alt="" /> */}
                     <div class="lenta__item_description">
                       <span>
-                        {item.text}
+                        {
+                          item.text.length > 50
+                          ?
+                          this.Services.functions.editText(item.text, {clear: true})
+                          :
+                          item.text
+                        }
                       </span>
                     </div>
                     <div class="lenta__item_statistic statistic">
@@ -47,6 +89,21 @@ export const display = function () {
           })
         }
       </div>
+      <button
+        onclick={() => {
+          let data = {
+            uuid: this.Variable.myInfo.uuid,
+            action: "add",
+            data: {
+              lang: "ru"
+            }
+          }
+          fetch(`/api/events/Posts?uuid=${this.Variable.myInfo.uuid}`, {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(data),
+          })
+        }}>1234</button>
     </div>
   )
 }
