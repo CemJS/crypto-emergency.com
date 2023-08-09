a03cc8ee8e8efaf38906d09665d8789db05e1395
ProstoEvgenii <98259817+ProstoEvgenii@users.noreply.github.com>
2023-08-09 12:50:57 +0300

    .

--- frontends/socialNetworks/display.tsx
+++ frontends/socialNetworks/display.tsx
@@ -24,86 +24,76 @@ const socialNetworks=[
     logo: instagram,
     regionRU: 'RU',
     regionENG: 'EN',
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
-    urlRU: '',
-    urlENG: '' 
+    loginRU: '@CryptoEmergencyRussia',
+    loginENG: '@CryptoEmergency',
+    urlRU: 'https://www.instagram.com/cryptoemergencyrussia/?hl=ru',
+    urlENG: 'https://www.instagram.com/cryptoemergency/?hl=ru' 
   },
 
-  {
-    logo: facebook,
-    regionRU: 'RU',
-    regionENG: 'EN',
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
-    urlRU: '',
-    urlENG: '' 
-  },
+  // {
+  //   logo: facebook,
+  //   regionRU: 'RU',
+  //   regionENG: 'EN',
+  //   loginRU: '@RUcryproemergency222',
+  //   loginENG: '@ENGcryproemergency222',
+  //   urlRU: '',
+  //   urlENG: '' 
+  // },
   {
     logo: tiktok,
     regionRU: 'RU',
     regionENG: 'EN',
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
-    urlRU: '',
-    urlENG: '' 
+    loginRU: '@cryptoemergencyrussia',
+    loginENG: '@cryptoemergency',
+    urlRU: 'https://www.tiktok.com/@cryptoemergencyrussia',
+    urlENG: 'https://www.tiktok.com/@cryptoemergency' 
   },
   {
     logo: youtube ,
     regionRU: 'RU',
     regionENG: 'EN',
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
-    urlRU: '',
-    urlENG: '' 
+    loginRU: '@СryptoEmergency',
+    loginENG: '@CryptoEmergency4865',
+    urlRU: 'https://www.youtube.com/channel/UCb9Fx-fNikzs-OZwnTXepLg/',
+    urlENG: 'https://www.youtube.com/channel/UCdDWOveIuvqkyusDK1gv4ig/' 
   },
   {
     logo: vk,
     regionRU: 'RU',
-    regionENG: 'EN',
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
-    urlRU: '',
+    regionENG: '',
+    loginRU: 'CryptoEmergency',
+    loginENG: '',
+    urlRU: 'https://vk.com/cryptoemergency',
     urlENG: ''
   },
   {
     logo: linkedin,
     regionENG: 'EN',
-    loginENG: '@ENGcryproemergency',
+    loginENG: 'CryproEmergency',
     urlENG: 'https://www.linkedin.com/company/86302977'
   },
   {
     logo: twitter,
-    regionRU: 'RU',
+    regionRU: '',
     regionENG: 'EN',
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
+    loginRU: '',
+    loginENG: '@cryptoemergency',
     urlRU: 'https://twitter.com/cryptoemergency',
     urlENG: ''  
   },
   {
     logo: discord,
     regionRU: 'RU',
-
-    loginRU: '@RUcryproemergency',
-    urlRU: 'https://github.com/CryptoEmergency',
+    loginRU: 'CryproEmergency',
+    urlRU: 'https://discord.com/invite/Qdm7W8DjYc',
     urlENG: ''
   },
-  {
-    logo: facebook,
-    regionRU: 'RU',
-    regionENG: 'EN',
 
-    loginRU: '@RUcryproemergency',
-    loginENG: '@ENGcryproemergency',
-    urlRU: 'https://github.com/CryptoEmergency',
-    urlENG: 'https://github.com/CryptoEmergency'
-  },
   {
     logo: github,
-    loginENG: '@ENGcryproemergency',
+    loginENG: '@CryptoEmergency',
     regionENG: 'EN',
-    url: 'https://github.com/CryptoEmergency'  
+    urlENG: 'https://github.com/CryptoEmergency'  
   },
 ]
 const apps=[
@@ -146,11 +136,11 @@ export const display = function () {
                         </div>
 
                         <div class="content">
-                          <a target="_blank" href={item.urlENG} class="ruLink">
+                          <a target="_blank" href={item.urlRU} class="ruLink">
                           <span class="spanRegionRU">{item.regionRU}</span>
                           {item.loginRU}
                           </a>
-                          <a target="_blank" href={item.urlRU} class="engLink">
+                          <a target="_blank" href={item.urlENG} class="engLink">
                           <span class="spanRegionENG">{item.regionENG}</span>
                           {item.loginENG} 
                           </a>
@@ -192,10 +182,19 @@ export const display = function () {
                   )
                 })
               }
-                
-
-
               </div>
+              <div class="title">
+               <h1>Наши проекты</h1>
+               <div class="apps-main_list">
+                  <a class="apps-main_list_item" target="_blank" href='https://cemscan.com/'>
+                    <div class="img">
+                      <img src={crypto_emergency} alt="img" />
+                      <p>CemScan</p>
+                    </div>
+                  </a>
+              </div>
+              </div>
+
 
             </div>
           </div>
