1afb4cdb33896d4850ead0b570c6c9294631de2f
maksdrop <maksondmb2018@gmail.com>
2023-08-11 11:05:32 +0300

    fix university

--- frontends/university/display.tsx
+++ frontends/university/display.tsx
@@ -3,7 +3,19 @@ import { Cemjsx } from "cemjs-all"
 export const display = function () {
   return (
     <div class="page">
-      university
+      <div class="university">
+        <div class="wrapper">
+          <div class="university__container">
+            <div class="university__header">
+              <div class="university__header_title">
+                <img src="" />
+                <p>Крипто университет</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
