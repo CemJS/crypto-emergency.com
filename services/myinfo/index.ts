60859a01b77e2c74ee792b706592be30f3f7acff
Betarost <betarost@yandex.ru>
2023-08-11 16:21:23 +0300

    add or validate

--- services/myinfo/index.ts
+++ services/myinfo/index.ts
@@ -2,21 +2,13 @@
 
 export const loader = async function (Variable) {
 
-    console.log('=97eada Variable.1 =', Variable.myInfo)
-
-
     let eventSource = new EventSource(`${Variable._Api}MyInfo?uuid=${Variable.myInfo.uuid}`)
 
     eventSource.addEventListener('message', ({ data }) => {
-        if (!data) {
+        if (!data || data == "null") {
             return
         }
         let records = JSON.parse(data)
-        console.log('=97eada Variable.myInfo =', Variable.myInfo)
-        console.log('=97eada records =', records)
-
         Variable.myInfo = Object.assign(Variable.myInfo, records)
-        console.log('=97eada Variable.myInfo assign =', Variable.myInfo)
-
     });
 }
