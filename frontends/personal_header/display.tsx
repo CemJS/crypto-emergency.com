9ce96e465ea3fef1a96766d3bdef801a44aa6aa3
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-08-11 17:26:04 +0300

    cover

--- frontends/personal_header/display.tsx
+++ frontends/personal_header/display.tsx
@@ -1,5 +1,6 @@
 import { Cemjsx } from "cemjs-all"
 import settings from '@svg/icon/settings.svg'
+import flag from '@svg/icon/flag.svg'
 
 export const display = function () {
   return (
@@ -13,12 +14,20 @@ export const display = function () {
           </div>
           <div class="personal_header_info">
             <div class="personal_header_info_wrap">
-              <div>logo</div>
-              <div class="personal_setting">
-                <span>
+
+              <div >logo</div>
+              <div class="personal_tools">
+                <div>
+                  <svg viewbox="-640 0 1400 150" width="15vw">
+                    <path id="heart" d="M110.84,180.14,25.52,94.82A50.12,50.12,0,0,1,96.4,23.94l14.44,14.44,14.44-14.44a50.12,50.12,0,0,1,70.88,70.88Z" />
+                  </svg>
+                </div>
+                <div class="personal_tools_img">
+                  <img src={flag} alt="Страна" />
+                </div>
+                <div class="personal_tools_img">
                   <img src={settings} alt="Настройки" />
-                </span>
-                <span>RU</span>
+                </div>
               </div>
             </div>
             {/* <div class="avatar"></div> */}
