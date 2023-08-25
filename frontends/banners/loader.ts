0a15ae68ea6226bc46b3f2522c988477a8c90c7f
maksdrop <maksondmb2018@gmail.com>
2023-08-25 11:59:09 +0300

    fix

--- frontends/banners/loader.ts
+++ frontends/banners/loader.ts
@@ -8,4 +8,13 @@ export const loader = function () {
     this.Static.records = records
     this.init()
   });
+
+  eventSource.addEventListener('add', ({ data }) => {
+    if (!this.Static.records) {
+      this.Static.records = []
+    }
+    let record = JSON.parse(data)
+    this.Static.records.push(record)
+    this.init()
+  });
 }
