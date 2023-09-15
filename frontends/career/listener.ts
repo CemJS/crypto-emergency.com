a5a38155aeb895479b130bf971968320cf2d437d
Betarost <betarost@yandex.ru>
2023-09-15 11:47:00 +0300

    .

--- frontends/career/listener.ts
+++ frontends/career/listener.ts
@@ -1,28 +1,23 @@
-const listener = {
-    "finish": function () {
-        this.Static.employments = document.querySelectorAll(".employment_item");
+export const finish = function () {
+    this.Static.employments = document.querySelectorAll(".employment_item");
 
-        const observer = new IntersectionObserver(entries => {
+    const observer = new IntersectionObserver(entries => {
 
-            entries.forEach(item => {
-                item.target.classList.toggle("show", item.isIntersecting)
-            })
-            // console.log('=b1055d=', entries)
+        entries.forEach(item => {
+            item.target.classList.toggle("show", item.isIntersecting)
         })
+        // console.log('=b1055d=', entries)
+    })
 
-        this.Static.employments.forEach(item => {
+    this.Static.employments.forEach(item => {
 
-            observer.observe(item)
-        })
+        observer.observe(item)
+    })
 
-        // this.Static.vacancy.forEach(item => {
-        //     observer.observe(item)
-        // })
+    // this.Static.vacancy.forEach(item => {
+    //     observer.observe(item)
+    // })
 
-        // observer.observe(this.Static.Ref.jdem)
-
-
-    }
+    // observer.observe(this.Static.Ref.jdem)
+    return
 }
-
-export { listener }
