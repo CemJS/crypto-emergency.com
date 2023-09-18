83204a3be6f4945537343caa9c6156e0f44f1925
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-09-18 15:40:05 +0300

    .

--- frontends/modalGallery/display.tsx
+++ frontends/modalGallery/display.tsx
@@ -1,43 +1,10 @@
 import { Cemjsx } from "cemjs-all"
-
+import Navigation from "./navigation"
 
 export const display = function () {
-  console.log('=727e5f=', this.Static.records[this.Static.activeIndex].name)
   return (
-    <div
-      class="modalWindow"
-      ref="modalWindow"
-      onclick={(e) => {
-        if (e.target === this.Ref.modalBody) {
-          setTimeout(() => {
-            this.clearData()
-          }, 5)
-        }
-      }}
-    >
-      <div class="modalWindow_body" ref="modalBody">
-        <div class="modalWindow_content modalWindow_content_gallery">
-          <button
-            class="modalWindow_button_close"
-            onclick={(e) => {
-              setTimeout(() => {
-                this.clearData()
-              }, 5)
-              this.Ref.modalWindow.classList.remove('activeModal');
-              this.Static.body.classList.remove('activeModal');
-              this.Static.body.style.overflow = 'auto';
-            }}
-          >X</button>
-
-          <main class="modalWindow_main">
-            <div class="modalGallery_wrap">
-              <div class="modalGallery_carousel">
-                <img src={`/assets/upload/worldPress/${this.Static.records[this.Static.activeIndex].name}`} />
-              </div>
-            </div>
-          </main>
-        </div>
-      </div>
+    <div class="page">
+      <Navigation />
     </div>
   )
 }
--- frontends/modalGallery/listener.ts
+++ frontends/modalGallery/listener.ts
@@ -7,33 +7,10 @@ export const finish = function (data, name, t) {
     return
 }
 
-const listener = {
-    "cross": [{
-        "front": "ico",
-        "fn": function ({ records }) {
-            console.log('=627aeb=', records)
-            this.Static.arr = records
-            this.init()
-        }
-    },
-        // {
-        //     "front": "modalSelectCountry",
-        //     "fn": function ({ name, code }) {
-        //         let splitName = name.split(" (")
-
-        //         this.Static.country.name = splitName[0]
-        //         this.Static.country.orig = splitName[1].replace(/[{()}]/g, '')
-        //         this.Static.country.code = code
-        //         this.Static.country.value = name
-        //         this.fn("checkForm")
-        //         this.init()
-        //     }
-        // }
-    ],
-    "start": function (data, name) {
-        // console.log('=e60c81=', data, name, this, this.Ref.tabs)
-    },
-
+const ico = function({ records }){
+    console.log('=627aeb=', records)
+    this.Static.arr = records
+    this.init()
 }
 
-export { listener }
+export const cross = [{ "front": "ico", fn: ico }]
