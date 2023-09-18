f8a3f1606f5c01b2fc1bf1ee456d579400967e0b
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-09-18 15:10:42 +0300

    .

--- frontends/modalRegistration/functions.ts
+++ frontends/modalRegistration/functions.ts
@@ -1,13 +1,9 @@
-const fn = {
-  "checkForm": function () {
-    if (this.Static.email.valid && this.Static.pass.valid && this.Static.repeatPass.valid && this.Static.check) {
-      this.Static.isValid = true
-    } else {
-      this.Static.isValid = false
-    }
+export const checkFrom = function () {
+  if (this.Static.email.valid && this.Static.pass.valid && this.Static.repeatPass.valid && this.Static.check) {
+    this.Static.isValid = true
+  } else {
+    this.Static.isValid = false
+  }
 
-    this.init()
-  },
+  this.init()
 }
-
-export { fn }
--- frontends/modalRegistration/listener.ts
+++ frontends/modalRegistration/listener.ts
@@ -1,10 +1,5 @@
-const listener = {
-
-  "finish": function (data, name, t) {
-      setTimeout(() => {
-          this.Static.body.style.overflow = 'hidden';
-      }, 100)
-  }
+export const finish = function (data, name, t) {
+  setTimeout(() => {
+    this.Static.body.style.overflow = 'hidden';
+  }, 100)
 }
-
-export { listener }
