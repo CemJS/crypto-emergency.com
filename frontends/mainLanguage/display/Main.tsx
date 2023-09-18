9a0dfd5a1134732a28c0d43bb435b51ab1b0f661
Betarost <betarost@yandex.ru>
2023-09-18 11:45:24 +0300

    .

--- frontends/mainLanguage/display/Main.tsx
+++ frontends/mainLanguage/display/Main.tsx
@@ -1,7 +1,119 @@
 import { Cemjsx } from "cemjs-all"
 
+const language = [
+    {
+        "lang": "Russian",
+        "lang_orig": "Русский",
+    },
+    {
+        "lang": "English",
+        "lang_orig": "English",
+    },
+    {
+        "lang": "China",
+        "lang_orig": "俄语",
+    },
+    {
+        "lang": "Deutsch",
+        "lang_orig": "Deutsch",
+    },
+    {
+        "lang": "Greek",
+        "lang_orig": "Ελληνικά",
+    },
+
+    {
+        "lang": "Spanish",
+        "lang_orig": "Español",
+    },
+    {
+        "lang": "French",
+        "lang_orig": "Français",
+    },
+    {
+        "lang": "Hebrew",
+        "lang_orig": "רובית",
+    },
+    {
+        "lang": "Armenian",
+        "lang_orig": "ռուսերեն",
+    },
+    {
+        "lang": "Indonesian",
+        "lang_orig": "Bahasa Indonesia",
+    },
+    {
+        "lang": "Italian",
+        "lang_orig": "Italiano",
+    },
+    {
+        "lang": "Japanese",
+        "lang_orig": "日本語",
+    },
+    {
+        "lang": "polish",
+        "lang_orig": "polszczyzna",
+    },
+    {
+        "lang": "Portuguese",
+        "lang_orig": "Português",
+    },
+
+    {
+        "lang": "Turkish",
+        "lang_orig": "Türkçe",
+    },
+    {
+        "lang": "Український",
+        "lang_orig": "Український",
+    }
+]
+
 export default function () {
     return (
-        <div></div>
+        <div class="modal modal_open">
+            <div class="modal__black"
+                onclick={() => {
+                    setTimeout(() => {
+                        this.Static.body.style.overflow = '';
+                        this.clearData()
+                    }, 5);
+                }}
+            />
+            <div class="modal__container language">
+                <header class="modal__header">
+                    <h4 class="language__title">
+                        Выбрать язык
+                    </h4>
+                    <button
+                        class="modal__close"
+                        type="button"
+                        onclick={() => {
+                            setTimeout(() => {
+                                this.Static.body.style.overflow = '';
+                                this.clearData()
+                            }, 5);
+                        }}
+                    />
+                </header>
+                <div class="modal__body language__container">
+                    <ul class="language__list">
+                        {
+                            language.map((item) => {
+                                return (
+                                    <li class="language__item">
+                                        <a>
+                                            <span>
+                                                {item.lang_orig}
+                                            </span>
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
---
+++ frontends/mainSettings/display/Main.tsx
@@ -0,0 +1,366 @@
+import { Cemjsx } from "cemjs-all"
+import lenta from '@svg/mainSettings/lenta.svg'
+import questions from '@svg/mainSettings/answers_questions.svg'
+import ico from '@svg/mainSettings/ico.svg'
+import startups from '@svg/mainSettings/startups.svg'
+import news from '@svg/mainSettings/news.svg'
+import exchanges from '@svg/mainSettings/exchanges.svg'
+import users from '@svg/mainSettings/users.svg'
+import university from '@svg/mainSettings/university.svg'
+import expert from '@svg/mainSettings/expert.svg'
+import contentmaker from '@svg/mainSettings/contentmaker.svg'
+import telegram from '@svg/mainSettings/telegram.svg'
+import youtube from '@svg/mainSettings/youtube.svg'
+import twitter from '@svg/mainSettings/twitter.svg'
+import discord from '@svg/mainSettings/discord.svg'
+import github from '@svg/mainSettings/github.svg'
+import tiktok from '@svg/mainSettings/tiktok.svg'
+import linkedin from '@svg/mainSettings/linkedin.svg'
+
+const sections = [
+    {
+        category: "Лента пользователей",
+        img: lenta,
+        url: "/lenta-users"
+    },
+    {
+        category: "Вопросы и ответы",
+        img: questions,
+        url: "/questions"
+    },
+    {
+        category: "ICO Рейтинг",
+        img: ico,
+        url: "/list-ico"
+    },
+    {
+        category: "Стартапы",
+        img: startups,
+        url: "/list-startaps"
+    },
+    {
+        category: "Новости",
+        img: news,
+        url: "/news"
+    },
+    {
+        category: "Обменники",
+        img: exchanges,
+        url: "/list-exchange"
+    },
+    {
+        category: "Пользователи",
+        img: users,
+        url: "/users"
+    },
+    {
+        category: "Крипто университет",
+        img: university,
+        url: "/university"
+    },
+    {
+        category: "Эксперты",
+        img: expert,
+        url: "#"
+    },
+    {
+        category: "Создатели контента",
+        img: contentmaker,
+        url: "#"
+    },
+]
+
+const socials = [
+    {
+        img: telegram,
+        url: "https://t.me/cryptoemergencychat"
+    },
+    {
+        img: youtube,
+        url: "https://www.youtube.com/channel/UCb9Fx-fNikzs-OZwnTXepLg/"
+    },
+    {
+        img: twitter,
+        url: "https://twitter.com/cryptoemergency"
+    },
+    {
+        img: discord,
+        url: "https://discord.com/invite/Qdm7W8DjYc"
+    },
+    {
+        img: github,
+        url: "https://github.com/CryptoEmergency"
+    },
+    {
+        img: tiktok,
+        url: "https://www.tiktok.com/@cryptoemergencyrussia"
+    },
+    {
+        img: linkedin,
+        url: "https://www.linkedin.com/company/86302977"
+    }
+]
+
+let collapseBodyShow = {
+    one: false,
+    two: false,
+    three: false,
+    four: false,
+};
+
+const showCollapseBody = function (e) {
+    // console.log('=107f5a=', e.target.dataset.number)
+    collapseBodyShow[e.target.dataset.number] = !collapseBodyShow[e.target.dataset.number];
+    e.stopPropagation();
+};
+
+export default function () {
+    return (
+        <div class="modal modal_open modal_fullscreen">
+            <div class="modal__black"
+                onclick={() => {
+                    setTimeout(() => {
+                        this.clearData()
+                    }, 5);
+                }}
+            />
+            <section class="main-menu main-menu_active">
+                <header class="main-menu__header">
+                    <div class="main-menu__btn">
+                        <div
+                            class="main-menu__btn_wrap"
+                            onclick={() => {
+                                this.clearData()
+                                this.Fn.initOne({
+                                    name: "mainServices"
+                                })
+                            }}
+                        >
+                            <span>Сервисы</span>
+                        </div>
+                    </div>
+                    <div
+                        class="main-menu__language"
+                        onclick={() => {
+                            this.Fn.initOne({
+                                name: "mainLanguage", ifOpen: (front) => {
+                                    // console.log('=9e3bb6=', front)
+                                    setTimeout(() => {
+                                        front.clearData()
+                                    }, 500);
+                                }
+                            })
+                        }}
+                    >
+                        <div class="main-menu__language_wrap">
+                            <div class="main-menu__language_control">
+                                <img class="language_change_world" src={`/assets/svg/mainSettings/language_world.svg`} /> Русский
+                            </div>
+                        </div>
+                    </div>
+                    <button
+                        type="button"
+                        class="main-menu__close"
+                        onclick={() => {
+                            this.$el.childNodes[1].classList.remove("main-menu_active")
+                            setTimeout(() => {
+                                this.clearData()
+                            }, 500);
+                        }}
+                    ></button>
+                </header>
+                <div class="main-menu__body">
+                    <div class="main-menu__title">
+                        <div class="main-menu__title_wrap">
+                            Разделы
+                        </div>
+                    </div>
+                    <div class="main-menu__content main-menu_main_content">
+                        {
+                            sections.map((item) => {
+                                return (
+                                    <a class="main-menu__link main-menu__link_visible"
+                                        href={item.url}
+                                        onclick={this.Fn.link}
+                                    >
+                                        <img src={item.img} />
+                                        <span class="mobile_user_menu_link">{item.category}</span>
+                                    </a>
+                                )
+                            })
+                        }
+
+                    </div>
+                    <div class="main-menu__title">
+                        <div class="main-menu__title_wrap">
+                            Crypto Emergency
+                        </div>
+                    </div>
+
+                    <div class="main-menu__footer">
+                        <div class="main-menu__footer_nav">
+                            <div class="main-menu__accordion accordion">
+                                <div class="accordion__header">
+                                    <button class={["accordion__button",
+                                        collapseBodyShow.one ? null : "accordion__button_rotate"
+                                    ]}
+                                        onclick={(e) => {
+                                            showCollapseBody(e)
+                                            this.init()
+                                        }}
+                                        data-number="one"
+                                    >
+                                        Компания
+                                    </button>
+                                </div>
+                                <div class="accordion__collapse"
+                                    style={!collapseBodyShow.one ? null : "max-height: 200px"}
+                                >
+                                    <div class="accordion__body">
+                                        <a class="accordion__link" href="/about" onclick={this.Fn.link}>
+                                            О нас
+                                        </a>
+                                        <a class="accordion__link" href="/career" onclick={this.Fn.link}>
+                                            Карьера
+                                        </a>
+                                        <a class="accordion__link" href="/assets/docs/LitePaperRu.pdf">
+                                            Lite Paper
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="main-menu__accordion accordion">
+                                <div class="accordion__header">
+                                    <button class={["accordion__button",
+                                        collapseBodyShow.two ? null : "accordion__button_rotate"
+                                    ]}
+                                        onclick={(e) => {
+                                            showCollapseBody(e)
+                                            this.init()
+                                        }}
+                                        data-number="two"
+                                    >
+                                        Поддержка
+                                    </button>
+                                </div>
+                                <div class="accordion__collapse"
+                                    style={!collapseBodyShow.two ? null : "max-height: 200px"}
+                                >
+                                    <div class="accordion__body">
+                                        <a class="accordion__link"
+                                            href="/contacts"
+                                            onclick={this.Fn.link}
+                                        >
+                                            Контакты
+                                        </a>
+                                        <a class="accordion__link" href="/personal/affiliate" onclick={this.Fn.link}>
+                                            Партнерская программа
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="main-menu__accordion accordion">
+                                <div class="accordion__header">
+                                    <button class={["accordion__button",
+                                        collapseBodyShow.three ? null : "accordion__button_rotate"
+                                    ]}
+                                        onclick={(e) => {
+                                            showCollapseBody(e)
+                                            this.init()
+                                        }}
+                                        data-number="three"
+                                    >
+                                        Правила
+                                    </button>
+                                </div>
+                                <div class="accordion__collapse"
+                                    style={!collapseBodyShow.three ? null : "max-height: 200px"}
+                                >
+                                    <div class="accordion__body">
+                                        <a class="accordion__link" href="/user-agreement" onclick={this.Fn.link}>
+                                            Пользовательское соглашение
+                                        </a>
+                                        <a class="accordion__link" href="/user-data-policy" onclick={this.Fn.link}>
+                                            Политика использования данных
+                                        </a>
+                                        <a class="accordion__link" href="/user-cookies" onclick={this.Fn.link}>
+                                            Политика cookies
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="main-menu__accordion accordion">
+                                <div class="accordion__header">
+                                    <button class={["accordion__button",
+                                        collapseBodyShow.four ? null : "accordion__button_rotate"
+                                    ]}
+                                        onclick={(e) => {
+                                            showCollapseBody(e)
+                                            this.init()
+                                        }}
+                                        data-number="four"
+                                    >
+                                        CEM
+                                    </button>
+                                </div>
+                                <div class="accordion__collapse"
+                                    style={!collapseBodyShow.four ? null : "max-height: 200px"}
+                                >
+                                    <div class="accordion__body">
+                                        <a
+                                            class="accordion__link"
+                                            target="_blank"
+                                            href="https://cemblockchain.com/"
+                                        >
+                                            CEM Blockchain
+                                        </a>
+                                        <a
+                                            class="accordion__link"
+                                            target="_blank"
+                                            href="https://cemscan.com/"
+                                        >
+                                            CEM Explorer
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="main-menu__accordion accordion">
+                                <a
+                                    href="https://apps.apple.com/ru/app/crypto-emergency/id1635628021"
+                                    target="_blank"
+                                    class="accordion__app"
+                                >
+                                    <img src={`/assets/svg/mainSettings/appstore.svg`} />
+                                </a>
+                                <a href="https://play.google.com/store/apps/details?id=com.cryptoemergency" target="_blank" class="accordion__app" style="margin-right: 0;">
+                                    <img src={`/assets/svg/mainSettings/googleplay.svg`} />
+                                </a>
+                            </div>
+                        </div>
+                        <div class="main-menu__footer_bottom">
+                            <div class="footer__socials">
+                                {
+                                    socials.map((item) => {
+                                        return (
+                                            <div class="footer__socials_link">
+                                                <a
+                                                    target="_blank"
+                                                    href={item.url}
+                                                >
+                                                    <img src={item.img} />
+                                                </a>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div class="footer__copyright">
+                                ©2020-2023 Crypto Emergency
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
