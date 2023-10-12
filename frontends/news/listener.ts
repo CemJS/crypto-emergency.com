731c307ddd57cda8cad59c964182022dd9f2f8a4
Betarost <betarost@yandex.ru>
2023-10-12 15:45:39 +0300

    .

--- frontends/news/listener.ts
+++ frontends/news/listener.ts
@@ -1,6 +1,6 @@
 const categoryLine = function ({ event, name }) {
-  this.fn("addEvent", { cat: name })
+  this.Static.records = []
+  this.Events.news.change(`/api/events/News?uuid=${this.Variable.myInfo.uuid}&lang=ru&cat=${name}`)
 }
 
 export const cross = [{ "front": "categoryLine", fn: categoryLine }]
-
--- frontends/news/loader.ts
+++ frontends/news/loader.ts
@@ -1,12 +1,12 @@
 export const loader = function () {
+    this.Static.records = []
     this.Events.news = this.event(`/api/events/News?uuid=${this.Variable.myInfo.uuid}&lang=ru`)
 
     this.Events.news.addEventListener('add', ({ data }) => {
-        if (!this.Static.records) {
-            this.Static.records = []
-        }
         let record = JSON.parse(data)
-        this.Static.records.push(record)
+        if (Object.keys(record).length) {
+            this.Static.records.push(record)
+        }
         this.init()
     });
 
