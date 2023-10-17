fcfd68d335b6919b8c57d3c0a13289f88ade107e
Betarost <betarost@yandex.ru>
2023-10-17 10:56:02 +0300

    .

--- frontends/news/loader.ts
+++ frontends/news/loader.ts
@@ -1,9 +1,9 @@
-import cateroguNews from "@json/categoryNews"
+import categoryNews from "@json/categoryNews"
 
 export const loader = function () {
     this.Static.records = []
     this.Static.record = null
-    this.Static.category = cateroguNews
+    this.Static.category = categoryNews
     this.Static.catActive = 0
     this.fn("addEvent")
     return
