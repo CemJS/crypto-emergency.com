2eeb4bbab0ff2a482151fdfd624e8fbbe58d3051
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-10-18 17:01:45 +0300

    .

--- frontends/modalAuthorization/functions.ts
+++ frontends/modalAuthorization/functions.ts
@@ -13,24 +13,14 @@ export const close = function () {
   }, 500)
 }
 
-// export const resetField = function (active: string) {
-//   this.Static.email.value = ""
-//   this.Static.email.valid = false
-//   this.Static.phone.value = ""
-//   this.Static.phone.valid = false
-//   this.fn("checkForm")
-//   this.Static.buttonActive = active
-// }
 
-// export const checkForm = function () {
-//   if (this.Static.pass.valid) {
-//     if (this.Static.email.valid || this.Static.phone.valid) {
-//       this.Static.isValid = true
-//     } else {
-//       this.Static.isValid = false
-//     }
-//   }
+export const checkForm = function () {
+  if (this.Static.form.email.valid && this.Static.form.pass.valid) {
+    this.Static.isValid = true
+  } else {
+    this.Static.isValid = false
+  }
 
-//   this.init()
-//   return
-// }
+  this.init()
+  return
+}
