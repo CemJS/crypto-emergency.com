3b554d1a354daf4ced008a4f6a445c16c7ebac5f
Betarost <betarost@yandex.ru>
2023-10-19 15:16:34 +0300

    .

--- frontends/about/navigation.tsx
+++ frontends/about/navigation.tsx
@@ -1,6 +1,20 @@
 import { Cemjsx } from "cemjs-all"
 import Main from "./display/Main"
+import UserAgreement from "./display/UserAgreement"
+import UserDataPolicy from "./display/UserDataPolicy"
+import UserCookies from "./display/UserCookies"
 
 export default function () {
-  return <Main />
+  let param: string = this.Variable.DataUrl[1]
+  switch (param) {
+    case 'agreement':
+      return <UserAgreement />
+    case 'policy':
+      return <UserDataPolicy />
+    case 'cookies':
+      return <UserCookies />
+    default:
+      return <Main />
+  }
 }
+
--- frontends/voting/display/Main.tsx
+++
@@ -1,9 +0,0 @@
-import { Cemjsx } from "cemjs-all"
-
-export default function () {
-    return (
-        <div class="page">
-            voting
-        </div>
-    )
-}
